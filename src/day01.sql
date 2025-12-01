8f0606a9754ba3699ddbd211033c4b6f73acfca2
Christoph Kober <kober.christoph@yahoo.com>
2025-12-01 21:42:53 +0100

    Day 1, Part 2

--- src/day01.sql
+++ src/day01.sql
@@ -16,7 +16,32 @@ rotations_with_raw_positions as (
     from rotations
 ),
 positions as (
-    select step, diff, case when raw_position < 0 then 100 + raw_position else raw_position end as position
+    select
+        step,
+        diff,
+        case when raw_position < 0 then 100 + raw_position else raw_position end as position
     from rotations_with_raw_positions
+),
+changes as (
+    select
+        step,
+        lag (position) over () as start_position,
+        diff as diff,
+        position as end_position
+    from positions
+),
+passes as (
+    select
+        start_position,
+        diff,
+        end_position,
+        case
+            when start_position + diff <= 0 then abs((start_position + diff) / 100) + (case when start_position = 0 then 0 else 1 end)
+            when start_position + diff >= 100 then (start_position + diff) / 100
+            when end_position = 0 then 0
+        end as passes
+    from changes
 )
-select (select count(*) from positions where position = 0) as solution_part_1;
+select
+    (select count(*) from passes where end_position = 0) as solution_part_1,
+    (select sum(passes) from passes) as solution_part_2;
